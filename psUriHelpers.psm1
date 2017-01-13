6e139e9c91344e8f580ffd05947ac568950e5c53
Øyvind Kallstad <kallstad@gmail.com>
2017-01-13 10:53:26 +0100

    Adds module structure

---
+++ psUriHelpers.psm1
@@ -0,0 +1,7 @@
+$Global:UriRegexPatteren = '(?:(?<scheme>\S+):)(?://)(?:(?<userinfo>(?:\S+)(?::\S*)?)@)?(?<host>[\u00A1-\uffff0-9a-z%][\u00A1-\uffff0-9a-z.%-]+(?<!\.))(?<path>/[\u00A1-\uffff0-9a-z_()/%]*)?(?<query>\?[\u00A1-\uffff0-9a-z=&%-]+)?(?<fragment>#[\u00A1-\uffff0-9a-z&=%-]+)?(?<port>:\d+)?(?:/)?'
+
+Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Functions') | ForEach-Object {
+    Get-ChildItem -Path $_.FullName | ForEach-Object {
+        . $_.FullName
+    }
+}
